ee263fd46cdee739b4428506f442acd3c88dba6e
Feldherren <rpaliwoda@googlemail.com>
2014-01-28 21:33:30 +0000

    Getting values from the config now works properly. SI-script functions again.

--- si.py
+++ si.py
@@ -1,28 +1,32 @@
 import willie
 import os
 
-pathOngoing = ""
-pathFinished = ""
-
 def configure(config):
     config.interactive_add("si", "pathOngoing", "Location of ongoing game folders?")
     config.interactive_add("si", "pathFinished", "Location of finished game folders?")
 
+def setup(bot):
+    if not bot.config.has_section('si'):
+        bot.say('SI not configured, make sure pathOngoing and pathFinished are defined')
+        return
+    if not bot.config.si.pathongoing.endswith('\\'):
+        bot.config.si.pathongoing = bot.config.si.pathongoing + '\\'
+    if not bot.config.si.pathfinished.endswith('\\'):
+        bot.config.si.pathfinished = bot.config.si.pathfinished + '\\'
+
 @willie.module.commands('check')
 def check(bot, trigger):
-    pathOngoing = bot.config.si.pathongoing
-    pathFinished = bot.config.si.pathfinished
-    if os.path.isdir(pathOngoing + trigger.group(2)):
+    if os.path.isdir(bot.config.si.pathongoing + trigger.group(2)):
         mainSave = ""
         turnFiles = {}
         ready = True
-        for file in os.listdir(pathOngoing + trigger.group(2)):
+        for file in os.listdir(bot.config.si.pathongoing + trigger.group(2)):
             if file.endswith(".trn"):
                 bot.say(file)
-                turnFiles[file] = os.stat(pathOngoing + trigger.group(2) + "\\" + file).st_mtime
+                turnFiles[file] = os.stat(bot.config.si.pathongoing + trigger.group(2) + "\\" + file).st_mtime
             elif file.endswith(".sav"):
                 bot.say(file)
-                mainSave = os.stat(pathOngoing + trigger.group(2) + "\\" + file).st_mtime
+                mainSave = os.stat(bot.config.si.pathongoing + trigger.group(2) + "\\" + file).st_mtime
         if len(turnFiles) > 0:
             for turn in turnFiles:
                 if turnFiles[turn] <= mainSave:
@@ -38,10 +42,15 @@ def check(bot, trigger):
 
 @willie.module.commands('ongoing')
 def ongoing(bot, trigger):
-    for dir in os.walk(pathOngoing).next()[1]:
+    for dir in os.walk(bot.config.si.pathongoing).next()[1]:
         bot.say(dir)
         
 @willie.module.commands('finished')
 def finished(bot, trigger):
-    for dir in os.walk(pathFinished).next()[1]:
+    for dir in os.walk(bot.config.si.pathfinished).next()[1]:
         bot.say(dir)
+
+@willie.module.commands('debug')
+def debug(bot, trigger):
+    bot.say("Ongoing games location:" + bot.config.si.pathongoing)
+    bot.say("Finished games location:" + bot.config.si.pathfinished)
